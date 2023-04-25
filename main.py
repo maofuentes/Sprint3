cdcc0d2707c396bd6a2cf94fd47c38cd347c615b
Franco Valdés <franco.v.navarro@gmail.com>
2023-04-24 20:59:25 -0400

    Función de contraseña

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
 from random import choice
 from string import ascii_letters, digits, punctuation
 
-usuarios = [
-        {"id":1,"nombre":"Miguel Garay Gallardo","telefono":98547621},
-        {"id":2,"nombre":"Franco Valdés Navarro", "telefono":36521478},
-        {"id":3,"nombre":"Viviana Vera Ceballos", "telefono":14785296},
-        {"id":4,"nombre":"Marcelo Fuentes Oliva", "telefono":36985214},
-]
-
-def validacion():
-    """
-    Funcion para validar que las opciones digitadas sean correctas
-    """
-    global opc_disponible
-    opcion_ingresada = input()
-    while True:
-        if opcion_ingresada in opc_disponible:
-            return opcion_ingresada 
-        else:
-            print("Opcion Ingresada no valida")
-            opcion_ingresada = input()
-
-
 def creador_contraseña():
     # Definir el número mínimo de caracteres:
     largo = 8
